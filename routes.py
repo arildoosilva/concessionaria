30d9b0280fc31cef862d9ade6b3f06316490f1fb
Arildo Oliveira <arildo.o.s@outlook.com>
2015-01-30 13:11:11 -0200

    Update routes.py
    Removed useless comments

--- routes.py
+++ routes.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
-################ routes proativ cars
 @app.route('/')
 def index():
 	return render_template('/index.php')
@@ -21,7 +20,7 @@ def login():
 @app.route('/revisao')
 def revisao():
 	return render_template('/revisao.php')
-################ routes proativ cars
+
 if __name__=='__main__':
 	app.debug = True
 	app.run()
